7750ec77a181b9c55f85df761ff2d8a1c380d253
Alevtina Ilina <ilina-ala@list.ru>
2024-03-05 01:43:46 +0300

    шифры

--- app/src/main/java/com/example/androidmessenger/CeasesActivity.java
+++ app/src/main/java/com/example/androidmessenger/CeasesActivity.java
@@ -43,8 +43,12 @@ public class CeasesActivity extends AppCompatActivity {
 
     public void encodeText() {
         String text = inputText.getText().toString();
+        String s = shiftAmount.getText().toString();
         if (TextUtils.isEmpty(text)) {
             inputText.setError("Field is empty!");
+            return;
+        } else if (TextUtils.isEmpty(s)) {
+            shiftAmount.setError("Field is empty!");
             return;
         } else {
             int shift = Integer.parseInt(shiftAmount.getText().toString());
@@ -66,8 +70,13 @@ public class CeasesActivity extends AppCompatActivity {
 
     public void decodeText() {
         String text = inputText.getText().toString();
+        String s = shiftAmount.getText().toString();
         if (TextUtils.isEmpty(text)) {
             inputText.setError("Field is empty!");
+            return;
+        }
+        else if (TextUtils.isEmpty(s)) {
+            shiftAmount.setError("Field is empty!");
             return;
         }
         else{
