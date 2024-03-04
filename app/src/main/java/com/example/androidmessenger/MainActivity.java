8536e4f84ee2359eda1c94fc551020c1fe21f1d3
Alevtina Ilina <ilina-ala@list.ru>
2024-03-04 21:32:56 +0300

    activity_code update

--- app/src/main/java/com/example/androidmessenger/MainActivity.java
+++ app/src/main/java/com/example/androidmessenger/MainActivity.java
@@ -43,12 +43,6 @@ public class MainActivity extends AppCompatActivity  implements NavigationView.O
         }
     }
 
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.bottom_navigation_menu, menu);
-        return true;
-    }
-
 
     public boolean onNavigationItemSelected(MenuItem item) {
 
